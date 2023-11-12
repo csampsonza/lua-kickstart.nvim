fcc560059161d731ccc505ecfc3d27ed6934031f
Craig Sampson <craig.sampson@safetyio.com>
2023-11-12 20:47:47 +0200

    Add undo . <leader>u

--- init.lua
+++ init.lua
@@ -218,7 +218,27 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "debugloop/telescope-undo.nvim",
+    },
+    config = function()
+      require("telescope").setup({
+        -- the rest of your telescope config goes here
+        extensions = {
+          undo = {
+            -- telescope-undo.nvim config, see below
+          },
+          -- other extensions:
+          -- file_browser = { ... }
+        },
+      })
+      require("telescope").load_extension("undo")
+      vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+    end,
+  },
   {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
