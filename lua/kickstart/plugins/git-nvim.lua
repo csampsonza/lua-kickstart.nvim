ac4c2f9c86253cb437e59b1d117dab351366da9a
Craig Sampson <craig.sampson@safetyio.com>
2024-04-25 08:18:02 +0200

    updating with better configs.  Remember brew install lazygit

--- lua/kickstart/plugins/git-nvim.lua
+++ lua/kickstart/plugins/git-nvim.lua
@@ -1,6 +1,16 @@
 return {
   {
+    'kdheepak/lazygit.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    keys = { { 'lg', ':LazyGitCurrentFile<Return>', silent = true } },
+    opts = {},
+  },
+
+  {
     'dinhhuy258/git.nvim',
+    event = 'BufReadPre',
     opts = {},
   },
 }
